9916e82483ac265e06a8593a9f6f30dd7b1d1b16
ElvinaNurmukhametova <elvina.nur@gmail.com>
2024-03-10 10:42:14 +0300

    final commits(from 10.03.2024)

--- project_0/game_v2_main_version.py
+++ project_0/game_v2_main_version.py
@@ -1,26 +1,60 @@
 import numpy as np
 import random
 random.seed()
-print(random.random)
-count = 0
-number = np.random.randint(1, 101)
-print("Загадано число от 1 до 100")
-min = 0
-max = 100
-while True:
-    predict = int(input())
-    count += 1
-    if number == predict:
-        break
-    elif number > predict:
-        min = predict
-        print(f"Угадываемое число больше {predict}")
-        print(f'Алгоритм бинарного поиска рекомендует вам число:{round((max + min) / 2)}')
-    elif number < predict:
-        max = predict
-        print(f"Угадываемое число меньше {predict}")
-        print(f'Алгоритм бинарного поиска рекомендует вам число:{round((max+min)/2)}')
+def guess_number(start, end, max_count):
+    print("Загадайте число от {start} до {end} я попытаюсь угадать его за {max_count} попыток или меньше.")
+    low = start
+    high = end
+    
+    for count in range(1, max_count + 1):
+        guess = random.randint(low, high)
+        
+        print(f'Попытка {count}: Я думаю ваше число {guess}.')
+        
+        if guess == hidden_number: 
+            print("Ура! Я угадал число!")
+            return
+                 
+        elif guess < hidden_number:
+            print("Мое число слишком маленькое.")
+            low = guess + 1
+            
+        else:
+            print("Мое число слишком большое.")
+            high = guess - 1
+            
+    print("К сожалению, я не смог угадать число за заданное количество попыток.")
+hidden_number = random.randint(1, 100)
+guess_number(1, 100, 20)                        
+           
+            
 
 
-print(f"Вы угадали число {number} за {count} попыток.")
+
+
+#import numpy as np
+#import random
+#random.seed()
+#print(random.random)
+#count = 0
+#number = np.random.randint(1, 101)
+#print("Загадано число от 1 до 100")
+#min = 0
+#max = 100
+#while True:
+    #predict = int(input())
+    #count += 1
+    #if number == predict:
+        #break
+    #elif number > predict:
+        #min = predict
+        #print(f"Угадываемое число больше {predict}")
+        #print(f'Алгоритм бинарного поиска рекомендует вам число:{round((max + min) / 2)}')
+    #elif number < predict:
+        #max = predict
+        #print(f"Угадываемое число меньше {predict}")
+        #print(f'Алгоритм бинарного поиска рекомендует вам число:{round((max+min)/2)}')
+
+
+#print(f"Вы угадали число {number} за {count} попыток.")
 
